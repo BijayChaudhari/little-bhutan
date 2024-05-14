1ada7f215bd2cbae194551c2cd727fc64d8ffbfe
Suresh Chand <scthakuri12a@gmail.com>
2024-05-14 16:45:47 +0545

    Fix Header

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,10 @@
-const hamburger = document.getElementById('hamburger');
-        const menu = document.getElementById('menu');
+$(document).on("click", "#hamburger", function(){
+    $(document).find("#drawer-example").toggleClass("-translate-x-full transform-none");
 
-        hamburger.addEventListener('click', () => {
-            menu.classList.toggle('hidden');
-        });
+    $("body").append('<div drawer-backdrop class="bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-30"></div>')
+});
+
+$(document).on("click", "div[drawer-backdrop], button[data-drawer-hide]", function(){
+    $(document).find("#drawer-example").toggleClass("-translate-x-full transform-none");
+    $(document).find("div[drawer-backdrop]").remove();
+})
