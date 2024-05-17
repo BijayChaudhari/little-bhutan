4a489febe60bb987e6cccbef676e037ccda284c3
Prakash Luitel <prakashluitel991@gmail.com>
2024-05-17 16:44:33 +0545

    added single url

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('contact_us/', views.contact, name="contact"),
     path('blogs/', views.blogs, name="blogs"),
     path('destinations/', views.destinations, name="destinations"),
+    path('single/', views.single, name="single"),
+
 ]
