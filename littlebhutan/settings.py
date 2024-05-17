5e3801ffb7ebab52a96125f3d6ee58a00693d629
Bijay Chaudhari <bijay.chaudhari1@gmail.com>
2024-05-17 11:48:41 +0545

    userauth

--- littlebhutan/settings.py
+++ littlebhutan/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+
+    #third party app
+    'userauth',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +111,21 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATIC_PATH = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "userauth.User"
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
